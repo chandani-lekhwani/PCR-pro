8f7c72cdec8346c271a49b505d1c98e6b2fcbb44
UED\uxdc-038 <chandani@peepaldesign.com>
2018-07-11 21:43:43 +0530

    Setup of ionic framework

---
+++ app/blank/constants/blank-config-const.js
@@ -0,0 +1,19 @@
+'use strict';
+angular.module('blank')
+.constant('BlankConfig', {
+
+  // gulp environment: injects environment vars
+  ENV: {
+    /*inject-env*/
+    'SERVER_URL': 'https://DEVSERVER/api',
+    'SOME_OTHER_URL': '/postman-proxy'
+    /*endinject*/
+  },
+
+  // gulp build-vars: injects build vars
+  BUILD: {
+    /*inject-build*/
+    /*endinject*/
+  }
+
+});
